d0432c85b372233e98d1091e16e07691e0c7c8c3
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2024-10-17 09:59:43 +0300

    added: transaction example

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Sequelize, Transaction } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('postgres_db', 'user', 'password', {
     host: 'localhost',
@@ -14,17 +14,49 @@ const connect = async () => {
     }
 }
 
-const setTransaction = async () => {
+const createTable = async () => {
     await sequelize.query(`
-        SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+        CREATE TABLE IF NOT EXISTS users (id SERIAL NOT NULL PRIMARY KEY, name varchar(40) NOT NULL, email varchar(40) NOT NULL);    
     `);
+}
 
-    console.log('Transaction isolation level set to REPEATABLE READ using raw SQL');
+// const setTransaction = async () => {
+//     await sequelize.query(`
+//         SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+//     `);
+
+//     console.log('Transaction isolation level set to REPEATABLE READ using raw SQL');
+// }
+
+const transactionExample = async () => {
+    const transaction = await sequelize.transaction();
+
+    try {
+        await sequelize.query(`
+            INSERT INTO users (name, email) VALUES ('John Doe', 'john.doe@example.com');
+        `, { transaction });
+
+        // await sequelize.query(`
+        //     UPDATE accounts SET balance = balance - 100 WHERE user_id = 1;
+        // `, { transaction });
+
+        // await sequelize.query(`
+        //     UPDATE accounts SET balance = balance + 100 WHERE user_id = 2;
+        // `, { transaction });
+
+        await transaction.commit();
+        console.log('Transaction has been committed successfully.');
+    } catch (error) {
+        await transaction.rollback();
+        console.error('Transaction has been rolled back due to an error:', error);
+    }
 }
 
 const run = async () => {
     await connect();
-    await setTransaction();
+    await createTable();
+    // await setTransaction();
+    await transactionExample();
 }
 
 run();
